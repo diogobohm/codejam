ae142c7a2e6ebab7eee4e72f48731c56f881552e
Diogo Böhm <diogo.bohm@gmail.com>
2013-11-06 23:34:28 -0200

    Finishing 2008-Round1A-C.

--- python/2008/2008-Round1A-C.py
+++ python/2008/2008-Round1A-C.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env python
 
-from mpmath import *
+from decimal import *                              # Improved math
 
+getcontext().prec = 1024                           # Adjust the precision
+base = (Decimal(3) + Decimal(5).sqrt())            # Calculate the base
 
-mp.dps = 100
-calc = mpf('5.2360679775')
+for case in range(1,int(raw_input())+1):           # For all test cases
+	exp = (int(raw_input()) - 3) % 100 + 3         # Get the recurrent exponent
 
+	num = str(base ** Decimal(exp))                # Calculate
+	ans = num[:num.find('.')]                      # Get only the integer part
 
-for case in range(1,int(raw_input())+1):
-	number = ((calc**mpf(raw_input())) % 1000)
-	print "Case #%d: %03d" % (case, number)
+	print "Case #%d: %03d" % (case, int(ans[-3:])) # Zero-lead the last 3 digits
